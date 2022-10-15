8e8f7d70c67830282a1f50ce734bf62106337427
Patryk Kugla <patryk.kugla@kcl.ac.uk>
2022-10-15 20:22:56 +0100

    Finish task

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    if number_of_primes > 0:
+        n = 2
+        while len(list) != number_of_primes:
+            prime = True
+            for number in list:
+                if n%number == 0:
+                    prime = False
+            if prime:
+                list.append(n)
+            n = n+1
+    else:
+        raise(Exception(ValueError))
     return list
